bb7daabfa7253a9c0ec6fe9c36e65e739a4d7627
jj <jillcarranza@gmail.com>
2024-10-26 03:29:53 -0600

    Update env.sv

--- env.sv
+++ env.sv
@@ -1,10 +1,16 @@
-class environment;
-  driver drvr;
-  scoreboard sb;
-  monitor mntr;
+class environment extends uvm_env;
+
+ `uvm_component_utils(environment)
+  
+  function new (string name = "environment", uvm_component parent = null);
+    super.new (name, parent);
+  endfunction
+
   virtual intf_wb intf;
            
-  function new(virtual intf_wb intf);
+  //.. continuar para modificarlo a UVM 
+  
+  /* function new(virtual intf_wb intf);
     $display("Creating environment");
     this.intf = intf;
     sb = new();
@@ -13,6 +19,7 @@ class environment;
     fork 
       mntr.check();
     join_none
-  endfunction
-           
+  endfunction*/
+  
+  
 endclass
