6d63357c07a1583aee972bd276d839eee3b3da26
JuanpaUM <juanpa.urema95@hotmail.com>
2024-10-28 22:31:56 -0600

    first_run

---
+++ exporting.sh
@@ -0,0 +1,5 @@
+export SNPSLMD_LICENSE_FILE=27020@172.21.99.41
+export SYNOPSYS_HOME=/mnt/vol_NFS_alajuela/qtree_NFS_rh003/synopsys_tools/synopsys
+export VCS_HOME=/mnt/vol_NFS_alajuela/qtree_NFS_rh003/synopsys_tools/synopsys/vcs-mx/O-2018.09-SP2-3
+export PATH=$PATH:$VCS_HOME/linux64/bin
+
