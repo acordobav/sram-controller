37e4d8066128639ca68b69604ae4203fc6c39a85
jj <jillcarranza@gmail.com>
2024-10-29 03:23:33 -0600

    Update virtual_seq.sv

--- proyecto2/virtual_seq.sv
+++ proyecto2/virtual_seq.sv
@@ -2,6 +2,9 @@ class virtual_sequence extends uvm_sequence;
   `uvm_object_utils(virtual_sequence)
   `uvm_declare_p_sequencer(virtual_sequencer)
   
+  rand logic [14:0] t_delay;
+  int num_repeats =2;
+  
   function new(string name="virtual_sequence");
     super.new(name);
   endfunction
@@ -22,17 +25,26 @@ class virtual_sequence extends uvm_sequence;
     seq1.randomize();
     seq1.start(p_sequencer.init_params_seqr);
     
+    t_delay=t_f(); // to randomize t_delay 
+    #t_delay;
+    
     seq2 = gen_reset_item_seq::type_id::create("seq2");
     
     seq2.randomize();
     seq2.start(p_sequencer.reset_seqr);
     
-    seq3 = gen_sram_item_seq::type_id::create("seq3");
+    t_delay=t_f(); // to randomize t_delay 
+    #t_delay;
     
-    seq3.randomize();
-    seq3.start(p_sequencer.sram_seqr);
-    
-    //#tiempo();
-    
+    for (int i = 0; i < num_repeats; i++) begin
+      seq3 = gen_sram_item_seq::type_id::create("seq3");
+      seq3.randomize();
+      seq3.start(p_sequencer.sram_seqr);
+    end
   endtask
 endclass
+
+
+function int t_f();
+    t_f = $urandom_range(1000, 10000); 
+endfunction
