8ad6bb8c6aa1b9731a58ce5a5b0232b1ccd30cac
VictorSanchezAlpizar <79665924+VictorSanchezAlpizar@users.noreply.github.com>
2024-11-19 08:55:51 -0600

    Update assertion.sv

--- proyecto3/assertion.sv
+++ proyecto3/assertion.sv
@@ -29,7 +29,34 @@ module assertions();
     (!`DUV_PATH.wb_rst_i) |-> ##1 (`DUV_PATH.wb_stb_i == 1'b0 && `DUV_PATH.wb_cyc_i == 1'b0);
   endproperty
   
-  clk_cycle_a: assert property (sync_clock_p) else $error("Rule1.1: Clock cycle assertion violated");
+  //WRITE AND READ ASSERTS
+  
+  property during_write_behavior_p;
+    @(posedge `DUV_PATH.sdram_clk)
+    disable iff (!`DUV_PATH.wb_rst_i)
+    (`DUV_PATH.wb_stb_i && `DUV_PATH.wb_cyc_i && `DUV_PATH.wb_we_i) |-> (`DUV_PATH.wb_addr_i != 1'bx && `DUV_PATH.wb_addr_i != 1'bz && `DUV_PATH.wb_dat_i != 1'bx && `DUV_PATH.wb_dat_i != 1'bz);
+  endproperty      
+  
+        
+  property after_write_behavior_p;
+    @(posedge `DUV_PATH.sdram_clk)
+    disable iff (!`DUV_PATH.wb_rst_i)
+    (`DUV_PATH.wb_ack_o) |-> ##1 (`DUV_PATH.wb_stb_i == 1'b0 && `DUV_PATH.wb_cyc_i == 1'b0 && `DUV_PATH.wb_we_i == 'hx && `DUV_PATH.wb_addr_i == 'hx && `DUV_PATH.wb_sel_i == 'hx && `DUV_PATH.wb_dat_i);
+  endproperty
+  
+  property during_read_behavior_p;
+    @(posedge `DUV_PATH.sdram_clk)
+    disable iff (!`DUV_PATH.wb_rst_i)
+    (`DUV_PATH.wb_ack_o && !`DUV_PATH.wb_we_i) |-> (`DUV_PATH.wb_addr_i != 1'bx && `DUV_PATH.wb_addr_i != 1'bz && `DUV_PATH.wb_dat_i != 1'bx && `DUV_PATH.wb_dat_i != 1'bz);
+  endproperty
+        
+  property after_read_behavior_p;
+    @(posedge `DUV_PATH.sdram_clk)
+    disable iff (!`DUV_PATH.wb_rst_i)
+    (`DUV_PATH.wb_ack_o) |-> ##1 (`DUV_PATH.wb_stb_i == 1'b0 && `DUV_PATH.wb_cyc_i == 1'b0 && `DUV_PATH.wb_we_i == 'hx && `DUV_PATH.wb_addr_i == 'hx);
+  endproperty
+  
+  clk_cycle_a: assert property (sync_clock_p) else $error("Rule1.1: Clock cycle assert violated");
     
   // Duracion de reset
   reset_duration_a: assert property (reset_duration_p) else $error("Rule3.05: WISHBONE RST signal must be asserted for at least one complete clock cycle");
@@ -40,5 +67,17 @@ module assertions();
   
   // Aserción para wb_stb_i y wb_cyc_i durante reset
   on_reset_behavior_a: assert property (on_reset_behavior_p) else $error("Rule3.21: WISHBONE signals wb_stb_i and wb_cyc_i must remain negated on reset");
+  
+  //Aserción durante WRITE operations
+  during_write_behavior_a: assert property (during_write_behavior_p) else $error("Rule4.20: WISHBONE signals (wb_addr_i, wb_dat_i) not in correct state while on write operation");
+    
+  //Aserción durante READ operations
+  during_read_behavior_a: assert property (during_read_behavior_p) else $error("Rule4.21: WISHBONE signals (wb_addr_i, wb_dat_i) not in correct state while on read operation");
+    
+  // Aserción para AFTER WRITE Behavior
+    after_write_behavior_a: assert property (after_write_behavior_p) else $error("Rule4.20: WISHBONE signals (wb_stb_i, wb_cyc_i, wb_we_i, wb_addr_i, wb_sel_i, wb_dat_i) not in correct state after write operation");
+    
+ // Aserción para AFTER READ Behavior
+      after_read_behavior_a: assert property (after_read_behavior_p) else $error("Rule4.21: WISHBONE signals (wb_stb_i, wb_cyc_i, wb_we_i, wb_addr_i) not in correct state after read operation");
   
 endmodule
