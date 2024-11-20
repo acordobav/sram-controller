b09dbb619cc98b4847e2f25c6b6a8410a4057324
Arturo Cordoba <arturocv16@gmail.com>
2024-11-19 18:06:59 -0600

    add banks coverage

--- proyecto3/coverage.sv
+++ proyecto3/coverage.sv
@@ -6,6 +6,7 @@ class funct_coverage extends uvm_component;
     cov_sel = new();
     cov_reset = new();
     cov_ACR = new();
+    cov_banks = new();
   endfunction
   
   virtual intf_wb intf;
@@ -24,6 +25,7 @@ class funct_coverage extends uvm_component;
         cov_sel.sample();
         cov_reset.sample();
         cov_ACR.sample();
+        cov_banks.sample();
       end
     end
   endtask
@@ -42,18 +44,28 @@ class funct_coverage extends uvm_component;
     }
   endgroup
   
+  covergroup cov_banks;
+    // Address Decodeing:
+    //  with cfg_col bit configured as: 00
+    //    <12 Bit Row> <2 Bit Bank> <8 Bit Column> <2'b00>
+    Feature_banks: coverpoint intf.wb_addr_i[11:10] {
+      bins possible_state[] = {2'b00, 2'b01, 2'b10, 2'b11};
+    }
+  endgroup
+
   covergroup cov_ACR;
     Feature_ACR: coverpoint intf.SDR_trcar_d {
       bins possible_state[] = {4'h1, 4'h2, 4'h3, 4'h4, 4'h5, 4'h6, 4'h7, 4'h8, 
                                4'h9, 4'ha, 4'hb, 4'hc, 4'hd, 4'he, 4'hf};
     }
   endgroup
-  
+
   virtual function void report_phase(uvm_phase phase);
     super.report_phase(phase);
     //Report coverage
     $display("COV SEL Overall: %3.2f%% coverage achieved.", cov_sel.get_coverage());
     $display("COV RST Overall: %3.2f%% coverage achieved.", cov_reset.get_coverage());
     $display("COV ACR Overall: %3.2f%% coverage achieved.", cov_ACR.get_coverage());
+    $display("COV BANKS Overall: %3.2f%% coverage achieved.", cov_banks.get_coverage());
   endfunction
 endclass
