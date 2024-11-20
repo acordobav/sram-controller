90739942df4e70d4ac43c27dbd50c6f132bb3aa2
JuanpaUM <juanpa.urema95@hotmail.com>
2024-11-19 18:55:17 -0600

    add coverage script

---
+++ proyecto3/coverage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+urg1 -full64 -dir simv.vdb
+
+firefox urgReport/dashboard.html
--- proyecto3/variables.sh
+++ proyecto3/variables.sh
@@ -3,3 +3,4 @@ export SYNOPSYS_HOME=/mnt/vol_NFS_alajuela/qtree_NFS_rh003/synopsys_tools/synops
 export VCS_HOME=/mnt/vol_NFS_alajuela/qtree_NFS_rh003/synopsys_tools/synopsys/vcs-mx/O-2018.09-SP2-3
 export PATH=$PATH:$VCS_HOME/linux64/bin
 export UVM_HOME=/mnt/vol_NFS_alajuela/qtree_NFS_rh003/synopsys_tools/synopsys/vcs-mx/O-2018.09-SP2-3/etc/uvm
+export LD_LIBRARY_PATH=$VCS_HOME/linux64/lib
